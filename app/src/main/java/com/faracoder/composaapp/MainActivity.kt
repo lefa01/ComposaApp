6cba323cd749e775301ff728e447c85c2efa957e
faracoder <faracoder91@gmail.com>
2023-11-16 20:19:30 +0400

    Initial commit

---
+++ app/src/main/java/com/faracoder/composaapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.faracoder.composaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import com.faracoder.composaapp.ui.theme.ComposaAppTheme
+import com.faracoder.composaapp.ui.theme.CustomComponent
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposaAppTheme{
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    var value by remember { mutableStateOf(0) }
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if (it.isNotEmpty()) {
+                                it.toInt()
+                            }else {
+                                0
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
